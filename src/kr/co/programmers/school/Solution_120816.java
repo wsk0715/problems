f7bfeb12e627890c706bec4a323be2dd704b68f0
wsk0715 <leindey@gmail.com>
2024-03-13 22:18:35 +0900

    ps: programmers 120816
    - lv.0 피자 나눠 먹기 (3)

---
+++ src/kr/co/programmers/school/Solution_120816.java
@@ -0,0 +1,19 @@
+package kr.co.programmers.school;
+
+public class Solution_120816 {
+    // 피자 나눠 먹기 (3)
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120816
+    public int solution(int slice, int n) {
+        int i = 0;
+
+        while (true) {
+            int x = slice * ++i / n;
+            if (x >= 1) {
+                break;
+            }
+        }
+
+        return i;
+    }
+
+}
