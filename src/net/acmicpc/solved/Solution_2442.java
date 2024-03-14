69be53b0d5cb87e80398395d76834dc7c1ef2ffd
wsk0715 <leindey@gmail.com>
2024-03-14 21:35:04 +0900

    ps: baekjoon 2442
    - bronze III : 별 찍기 - 5

---
+++ src/net/acmicpc/solved/Solution_2442.java
@@ -0,0 +1,25 @@
+package net.acmicpc.solved;
+
+import java.util.Scanner;
+
+public class Solution_2442 {
+    // 별 찍기 - 5
+    // https://www.acmicpc.net/problem/2442
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        for (int i = 1; i <= input; i++) {
+            for (int j = input - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
