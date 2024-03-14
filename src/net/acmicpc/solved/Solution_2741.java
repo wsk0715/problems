f6c9cf97a31acc13ff7a25a12df221c4d66859b9
wsk0715 <leindey@gmail.com>
2024-03-14 21:38:24 +0900

    ps: baekjoon 2741
    - bronze V : N 찍기

---
+++ src/net/acmicpc/solved/Solution_2741.java
@@ -0,0 +1,20 @@
+package net.acmicpc.solved;
+
+import java.util.Scanner;
+
+public class Solution_2741 {
+    // N 찍기
+    // https://www.acmicpc.net/problem/2741
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        for (int i = 0; i < input; i++) {
+            System.out.println(i + 1);
+        }
+
+    }
+
+}
+
