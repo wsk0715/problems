c418d5c2d3310fb23a0230fe2d448622f6813d7c
wsk0715 <leindey@gmail.com>
2024-03-16 14:36:22 +0900

    etc: organized packages

---
+++ src/kr/co/programmers/school/solving/$Solution_.java
@@ -0,0 +1,7 @@
+package kr.co.programmers.school.solving;
+
+public class $Solution_ {
+    //
+    // https://school.programmers.co.kr/learn/courses/30/lessons/
+
+}
---
+++ src/net/acmicpc/solving/$Solution_.java
@@ -0,0 +1,15 @@
+package net.acmicpc.solving;
+
+import java.util.Scanner;
+
+public class $Solution_ {
+    //
+    // https://www.acmicpc.net/problem/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+    }
+
+}
+
