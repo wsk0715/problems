86b0037c0b6c42c6649bc226ce54547d7b02dd8a
wsk0715 <leindey@gmail.com>
2024-03-19 23:37:16 +0900

    etc: update ps template

--- src/net/acmicpc/solving/$Solution_.java
+++ src/net/acmicpc/solving/$Solution_.java
@@ -1,15 +1,37 @@
 package net.acmicpc.solving;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class $Solution_ {
+    private static final Scanner sc = new Scanner(System.in);
+
     //
     // https://www.acmicpc.net/problem/
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int n = readInt();
 
-        String input = sc.nextLine();
+    }
+
+
+    // Libraries
+    private static String readLine() {
+        return sc.nextLine();
+    }
+
+    private static int readInt() {
+        int value = sc.nextInt();
+        sc.nextLine();
+        return value;
+    }
+
+    private static int[] readIntFrom(int n) {
+        for (int i = 1; i < n; i++) {
+            sc.nextInt();
+        }
+        return Arrays.stream(sc.nextLine().trim().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 
 }
-
