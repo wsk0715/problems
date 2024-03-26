7e30f43790463b3ac230430c293f78d0c43b72f9
wsk0715 <leindey@gmail.com>
2024-03-26 22:51:15 +0900

    etc: update ps template

--- src/kr/co/programmers/school/solving/$Solution_.java
+++ src/kr/co/programmers/school/solving/$Solution_.java
@@ -1,7 +1,22 @@
 package kr.co.programmers.school.solving;
 
 public class $Solution_ {
-    //
-    // https://school.programmers.co.kr/learn/courses/30/lessons/
+	//
+	// https://school.programmers.co.kr/learn/courses/30/lessons/
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int solution(int[] n) {
+			int answer = 0;
+
+			return answer;
+		}
+
+	}
 
 }
