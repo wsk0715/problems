fae975bb96f4a1c07fb9179d9e890f5663d2e760
wsk0715 <leindey@gmail.com>
2024-03-26 22:48:16 +0900

    ps: programmers 120898
    - lv.0 편지

---
+++ src/kr/co/programmers/school/solved/Solution_120898.java
@@ -0,0 +1,20 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_120898 {
+	// 편지
+	// https://school.programmers.co.kr/learn/courses/30/lessons/120898
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int solution(String message) {
+			return message.length() * 2;
+		}
+
+	}
+
+}
