16bf4a5f6a877c593e95e1e8579cc952bfac01a9
wsk0715 <leindey@gmail.com>
2024-03-26 22:49:18 +0900

    ps: programmers 120903
    - lv.0 배열의 유사도

---
+++ src/kr/co/programmers/school/solved/Solution_120903.java
@@ -0,0 +1,29 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_120903 {
+	// 배열의 유사도
+	// https://school.programmers.co.kr/learn/courses/30/lessons/120903
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int solution(String[] s1, String[] s2) {
+			int count = 0;
+
+			for (int i = 0; i < s1.length; i++) {
+				for (int j = 0; j < s2.length; j++) {
+					if (s1[i].equals(s2[j])) {
+						count++;
+					}
+				}
+			}
+			return count;
+		}
+
+	}
+
+}
