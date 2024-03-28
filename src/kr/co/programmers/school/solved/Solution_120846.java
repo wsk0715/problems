a433f8cd4bb1478ab488218437096fbeecdbba9a
wsk0715 <leindey@gmail.com>
2024-03-28 18:44:37 +0900

    ps: programmers 120846
    - lv.0 합성수 찾기

---
+++ src/kr/co/programmers/school/solved/Solution_120846.java
@@ -0,0 +1,36 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_120846 {
+	// 합성수 찾기
+	// https://school.programmers.co.kr/learn/courses/30/lessons/120846
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int solution(int n) {
+			int count = 0;
+			for (int i = 1; i <= n; i++) {
+				if (getDivs(i) >= 3) {
+					count++;
+				}
+			}
+			return count;
+		}
+
+		public int getDivs(int n) {
+			int count = 0;
+			for (int i = 1; i <= n; i++) {
+				if (n % i == 0) {
+					count++;
+				}
+			}
+			return count;
+		}
+
+	}
+
+}
