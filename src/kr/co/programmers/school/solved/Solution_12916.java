ca52b4c430c86810b27751856ecf0ef31313d4d2
wsk0715 <leindey@gmail.com>
2024-03-29 22:49:19 +0900

    ps: programmers 12916
    - lv.1 문자열 내 p와 y의 개수

---
+++ src/kr/co/programmers/school/solved/Solution_12916.java
@@ -0,0 +1,33 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_12916 {
+	// lv.1 문자열 내 p와 y의 개수
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12916
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		boolean solution(String s) {
+			int np = 0;
+			int ny = 0;
+
+			s = s.toLowerCase();
+			for (int i = 0; i < s.length(); i++) {
+				if (s.charAt(i) == 'p') {
+					np++;
+					continue;
+				}
+				if (s.charAt(i) == 'y') {
+					ny++;
+				}
+			}
+			return (np == ny) ? true : false;
+		}
+
+	}
+
+}
