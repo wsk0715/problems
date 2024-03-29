0af14f8e21f8f2a6236ab6529933dfa64627c456
wsk0715 <leindey@gmail.com>
2024-03-29 22:52:16 +0900

    ps: programmers 70128
    - lv.1 내적

---
+++ src/kr/co/programmers/school/solved/Solution_70128.java
@@ -0,0 +1,24 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_70128 {
+	// 내적 - lv.1
+	// https://school.programmers.co.kr/learn/courses/30/lessons/70128
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int solution(int[] a, int[] b) {
+			int sum = 0;
+			for (int i = 0; i < a.length; i++) {
+				sum += a[i] * b[i];
+			}
+			return sum;
+		}
+
+	}
+
+}
