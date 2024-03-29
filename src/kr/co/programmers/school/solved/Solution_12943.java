f77068070049b554512de51e362a7dfc6c6815a7
wsk0715 <leindey@gmail.com>
2024-03-29 22:51:18 +0900

    ps: programmers 12943
    - lv.1 콜라츠 추측

---
+++ src/kr/co/programmers/school/solved/Solution_12943.java
@@ -0,0 +1,31 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_12943 {
+	// 콜라츠 추측 - lv.1
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12943
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int solution(long n) {
+			for (int i = 0; i < 500; i++) {
+				if (n == 1) {
+					return i;
+				}
+				if (n % 2 == 0) {
+					n /= 2;
+				} else {
+					n *= 3;
+					n += 1;
+				}
+			}
+			return -1;
+		}
+
+	}
+
+}
