8d148795b28c573db90cfb453d67d64ab0cb1c3f
wsk0715 <leindey@gmail.com>
2024-03-29 22:51:28 +0900

    ps: programmers 12944
    - lv.1 평균 구하기

---
+++ src/kr/co/programmers/school/solved/Solution_12944.java
@@ -0,0 +1,24 @@
+package kr.co.programmers.school.solved;
+
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
+public class Solution_12944 {
+	// 평균 구하기 - lv.1
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12944
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public double solution(int[] arr) {
+			OptionalDouble avg = Arrays.stream(arr).average();
+			return avg.getAsDouble();
+		}
+
+	}
+
+}
