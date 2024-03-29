5c4edd8e7bd819510714c47dde1bd989c84b8c29
wsk0715 <leindey@gmail.com>
2024-03-29 22:51:10 +0900

    ps: programmers 12937
    - lv.1 짝수와 홀수

---
+++ src/kr/co/programmers/school/solved/Solution_12937.java
@@ -0,0 +1,23 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_12937 {
+	// 짝수와 홀수 - lv.1
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12937
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public String solution(int n) {
+			if (n % 2 == 0) {
+				return "Even";
+			}
+			return "Odd";
+		}
+
+	}
+
+}
