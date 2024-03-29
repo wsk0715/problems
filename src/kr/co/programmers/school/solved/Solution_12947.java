f7523c98da34ec7293c5b6d3ed7c5c2609786533
wsk0715 <leindey@gmail.com>
2024-03-29 22:51:42 +0900

    ps: programmers 12947
    - lv.1 하샤드 수

---
+++ src/kr/co/programmers/school/solved/Solution_12947.java
@@ -0,0 +1,25 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_12947 {
+	// 하샤드 수 - lv.1
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12947
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public boolean solution(int x) {
+			String s = String.valueOf(x);
+			int sum = 0;
+			for (int i = 0; i < s.length(); i++) {
+				sum += s.charAt(i) - 48;
+			}
+			return x % sum == 0 ? true : false;
+		}
+
+	}
+
+}
