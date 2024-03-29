9d52d24c68359281b0fd60e1d58383ea4d4f9dda
wsk0715 <leindey@gmail.com>
2024-03-29 22:53:20 +0900

    ps: programmers 87389
    - lv.1 나머지가 1이 되는 수 찾기

---
+++ src/kr/co/programmers/school/solved/Solution_87389.java
@@ -0,0 +1,26 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_87389 {
+	// 나머지가 1이 되는 수 찾기 - lv.1
+	// https://school.programmers.co.kr/learn/courses/30/lessons/87389
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int solution(int n) {
+			int m = n - 1;
+			for (int i = 2; i <= m; i++) {
+				if (m % i == 0) {
+					return i;
+				}
+			}
+			return 0;
+		}
+
+	}
+
+}
