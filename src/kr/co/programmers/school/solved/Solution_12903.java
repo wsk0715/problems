26695e187dd5077e5ab8e12a250a71c6a6ba1930
wsk0715 <leindey@gmail.com>
2024-03-29 22:48:35 +0900

    ps: programmers 12903
    - lv.1 가운데 글자 가져오기

---
+++ src/kr/co/programmers/school/solved/Solution_12903.java
@@ -0,0 +1,24 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_12903 {
+	// 가운데 글자 가져오기 - lv.1
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12903
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public String solution(String s) {
+			if (s.length() % 2 == 0) {
+				return s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+			} else {
+				return s.substring(s.length() / 2, s.length() / 2 + 1);
+			}
+		}
+
+	}
+
+}
