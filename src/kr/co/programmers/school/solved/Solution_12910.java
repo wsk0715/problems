55d95d83c54a684905357574738427a4039ead23
wsk0715 <leindey@gmail.com>
2024-03-29 22:48:48 +0900

    ps: programmers 12910
    - lv.1 나누어 떨어지는 숫자 배열

---
+++ src/kr/co/programmers/school/solved/Solution_12910.java
@@ -0,0 +1,40 @@
+package kr.co.programmers.school.solved;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution_12910 {
+	// 나누어 떨어지는 숫자 배열 - lv.1
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12910
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int[] solution(int[] arr, int d) {
+			ArrayList<Integer> list = new ArrayList<>();
+			for (int n : arr) {
+				if (n % d == 0) {
+					list.add(n);
+				}
+			}
+			Collections.sort(list);
+			int[] result;
+			if (list.size() == 0) {
+				return new int[]{-1};
+			} else {
+				result = new int[list.size()];
+				for (int i = 0; i < result.length; i++) {
+					result[i] = list.get(i);
+				}
+				return result;
+			}
+
+		}
+
+	}
+
+}
