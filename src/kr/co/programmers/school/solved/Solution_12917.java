1005372bd8dd4f4467ed899103a11a972627855f
wsk0715 <leindey@gmail.com>
2024-03-29 22:49:29 +0900

    ps: programmers 12917
    - lv.1 문자열 배치하기

---
+++ src/kr/co/programmers/school/solved/Solution_12917.java
@@ -0,0 +1,31 @@
+package kr.co.programmers.school.solved;
+
+import java.util.Arrays;
+
+public class Solution_12917 {
+	// 문자열 배치하기 - lv.1
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12917
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public String solution(String s) {
+			char[] arr = new char[s.length()];
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = s.charAt(i);
+			}
+			Arrays.sort(arr);
+			StringBuilder sb = new StringBuilder();
+			for (int i = arr.length - 1; i >= 0; i--) {
+				sb.append(arr[i]);
+			}
+			return sb.toString();
+		}
+
+	}
+
+}
