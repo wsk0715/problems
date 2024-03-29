a825d69253abb244fffef4387cd5995b4e4b5d60
wsk0715 <leindey@gmail.com>
2024-03-29 22:49:05 +0900

    ps: programmers 12912
    - lv.1 두 정수 사이의 합

---
+++ src/kr/co/programmers/school/solved/Solution_12912.java
@@ -0,0 +1,26 @@
+package kr.co.programmers.school.solved;
+
+public class Solution_12912 {
+	// 두 정수 사이의 합 - lv.1
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12912
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public long solution(int a, int b) {
+			int small = Math.min(a, b);
+			int big = Math.max(a, b);
+			long sum = 0;
+			for (int i = small; i <= big; i++) {
+				sum += i;
+			}
+			return sum;
+		}
+
+	}
+
+}
