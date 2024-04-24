5e6e7647136fb1e5958d2c53d9c5d28a3871560b
wsk0715 <leindey@gmail.com>
2024-04-24 20:46:52 +0900

    ps: programmers 181875
    - lv.0 배열에서 문자열 대소문자 변환하기

---
+++ src/kr/co/programmers/school/solved/Solution_181875.java
@@ -0,0 +1,40 @@
+package kr.co.programmers.school.solved;
+
+import java.util.ArrayList;
+
+public class Solution_181875 {
+	// 배열에서 문자열 대소문자 변환하기
+	// https://school.programmers.co.kr/learn/courses/30/lessons/181875
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public String[] solution(String[] arr) {
+			for (int i = 0; i < arr.length; i++) {
+				if (i % 2 != 0) {
+					arr[i] = arr[i].toUpperCase();
+					continue;
+				}
+				arr[i] = arr[i].toLowerCase();
+			}
+			return arr;
+		}
+
+
+		// Libraries
+		private int[] toArray(ArrayList<Integer> al) {
+			// ArrayList<Integer> to int[]
+			int[] arr = new int[al.size()];
+			for (int i = 0; i < al.size(); i++) {
+				arr[i] = al.get(i);
+			}
+			return arr;
+		}
+
+	}
+
+}
