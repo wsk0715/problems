e4edff6bb7af0563b1bdcf7bc0e451607b2c8973
wsk0715 <leindey@gmail.com>
2024-04-25 21:57:09 +0900

    ps: programmers 181899
    - lv.0 카운트 다운

---
+++ src/kr/co/programmers/school/solved/Solution_181899.java
@@ -0,0 +1,37 @@
+package kr.co.programmers.school.solved;
+
+import java.util.ArrayList;
+
+public class Solution_181899 {
+	// 카운트 다운
+	// https://school.programmers.co.kr/learn/courses/30/lessons/181899
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int[] solution(int start, int end_num) {
+			ArrayList<Integer> al = new ArrayList<>();
+			for (int i = start; i >= end_num; i--) {
+				al.add(i);
+			}
+			return toArray(al);
+		}
+
+
+		// Libraries
+		private int[] toArray(ArrayList<Integer> al) {
+			// ArrayList<Integer> to int[]
+			int[] arr = new int[al.size()];
+			for (int i = 0; i < al.size(); i++) {
+				arr[i] = al.get(i);
+			}
+			return arr;
+		}
+
+	}
+
+}
