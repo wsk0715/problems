bc90bf6eab06f5b262188c38751dde9fc507bc0c
wsk0715 <leindey@gmail.com>
2024-04-26 17:29:54 +0900

    ps: programmers 120802
    - lv.0 두 수의 합

---
+++ src/kr/co/programmers/school/solved/beginner/Solution_120802.java
@@ -0,0 +1,33 @@
+package kr.co.programmers.school.solved.beginner;
+
+import java.util.ArrayList;
+
+public class Solution_120802 {
+	// 두 수의 합
+	// https://school.programmers.co.kr/learn/courses/30/lessons/120802
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int solution(int num1, int num2) {
+			return num1 + num2;
+		}
+
+
+		// Libraries
+		private int[] toArray(ArrayList<Integer> al) {
+			// ArrayList<Integer> to int[]
+			int[] arr = new int[al.size()];
+			for (int i = 0; i < al.size(); i++) {
+				arr[i] = al.get(i);
+			}
+			return arr;
+		}
+
+	}
+
+}
