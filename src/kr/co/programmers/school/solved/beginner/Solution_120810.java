f0978ec4b8bb4c2e30cadfea7c25be3800e10f2e
wsk0715 <leindey@gmail.com>
2024-04-26 17:31:06 +0900

    ps: programmers 120810
    - lv.0 나머지 구하기

---
+++ src/kr/co/programmers/school/solved/beginner/Solution_120810.java
@@ -0,0 +1,20 @@
+package kr.co.programmers.school.solved.beginner;
+
+public class Solution_120810 {
+	// 나머지 구하기
+	// https://school.programmers.co.kr/learn/courses/30/lessons/120810
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int solution(int num1, int num2) {
+			return num1 % num2;
+		}
+
+	}
+
+}
