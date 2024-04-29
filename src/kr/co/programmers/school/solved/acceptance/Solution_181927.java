d77747184cc0d25e710818bde1f1b03d1122a377
wsk0715 <leindey@gmail.com>
2024-04-29 22:32:12 +0900

    ps: programmers 181927
    - lv.0 마지막 두 원소

---
+++ src/kr/co/programmers/school/solved/acceptance/Solution_181927.java
@@ -0,0 +1,44 @@
+package kr.co.programmers.school.solved.acceptance;
+
+import java.util.ArrayList;
+
+public class Solution_181927 {
+	// 마지막 두 원소
+	// https://school.programmers.co.kr/learn/courses/30/lessons/181927
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int[] solution(int[] arr) {
+			int[] r = new int[arr.length + 1];
+			for (int i = 0; i < arr.length; i++) {
+				r[i] = arr[i];
+			}
+
+			int l = arr.length - 1;
+			if (r[l] > r[l - 1]) {
+				r[l + 1] = r[l] - r[l - 1];
+				return r;
+			}
+			r[l + 1] = r[l] * 2;
+			return r;
+		}
+
+
+		// Libraries
+		private int[] toArray(ArrayList<Integer> al) {
+			// ArrayList<Integer> to int[]
+			int[] arr = new int[al.size()];
+			for (int i = 0; i < al.size(); i++) {
+				arr[i] = al.get(i);
+			}
+			return arr;
+		}
+
+	}
+
+}
