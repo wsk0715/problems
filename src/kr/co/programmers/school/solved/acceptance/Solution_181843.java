aa1abcb380f967098058cc27d2d4a7561e984991
wsk0715 <leindey@gmail.com>
2024-05-03 23:59:40 +0900

    ps: programmers 181843
    - lv.0 부분 문자열인지 확인하기

---
+++ src/kr/co/programmers/school/solved/acceptance/Solution_181843.java
@@ -0,0 +1,33 @@
+package kr.co.programmers.school.solved.acceptance;
+
+import java.util.ArrayList;
+
+public class Solution_181843 {
+	// 부분 문자열인지 확인하기
+	// https://school.programmers.co.kr/learn/courses/30/lessons/181843
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int solution(String s, String t) {
+			return s.contains(t) ? 1 : 0;
+		}
+
+
+		// Libraries
+		private int[] toArray(ArrayList<Integer> al) {
+			// ArrayList<Integer> to int[]
+			int[] arr = new int[al.size()];
+			for (int i = 0; i < al.size(); i++) {
+				arr[i] = al.get(i);
+			}
+			return arr;
+		}
+
+	}
+
+}
