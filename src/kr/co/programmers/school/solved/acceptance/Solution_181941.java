1f6deee7e420bb5156c790d3dff82d49938f2ae4
wsk0715 <leindey@gmail.com>
2024-05-06 19:15:52 +0900

    ps: programmers 181941
    - lv.0 문자 리스트를 문자열로 반환하기

---
+++ src/kr/co/programmers/school/solved/acceptance/Solution_181941.java
@@ -0,0 +1,33 @@
+package kr.co.programmers.school.solved.acceptance;
+
+import java.util.ArrayList;
+
+public class Solution_181941 {
+	// 문자 리스트를 문자열로 반환하기
+	// https://school.programmers.co.kr/learn/courses/30/lessons/181941
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public String solution(String[] arr) {
+			return String.join("", arr);
+		}
+
+
+		// Libraries
+		private int[] toArray(ArrayList<Integer> al) {
+			// ArrayList<Integer> to int[]
+			int[] arr = new int[al.size()];
+			for (int i = 0; i < al.size(); i++) {
+				arr[i] = al.get(i);
+			}
+			return arr;
+		}
+
+	}
+
+}
