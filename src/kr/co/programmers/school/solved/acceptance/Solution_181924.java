4efe26c48291471eff6ca9675a1ce59b1a523d5a
wsk0715 <leindey@gmail.com>
2024-05-08 20:58:57 +0900

    ps: programmers 181924
    - lv.0 수열과 구간 쿼리 3

---
+++ src/kr/co/programmers/school/solved/acceptance/Solution_181924.java
@@ -0,0 +1,39 @@
+package kr.co.programmers.school.solved.acceptance;
+
+import java.util.ArrayList;
+
+public class Solution_181924 {
+	// 수열과 구간 쿼리 3
+	// https://school.programmers.co.kr/learn/courses/30/lessons/181924
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public int[] solution(int[] arr, int[][] queries) {
+			for (int i = 0; i < queries.length; i++) {
+				int a = queries[i][0];
+				int b = queries[i][1];
+				int tmp = arr[a];
+				arr[a] = arr[b];
+				arr[b] = tmp;
+			}
+			return arr;
+		}
+
+		// Libraries
+		private int[] toArray(ArrayList<Integer> al) {
+			// ArrayList<Integer> to int[]
+			int[] arr = new int[al.size()];
+			for (int i = 0; i < al.size(); i++) {
+				arr[i] = al.get(i);
+			}
+			return arr;
+		}
+
+	}
+
+}
