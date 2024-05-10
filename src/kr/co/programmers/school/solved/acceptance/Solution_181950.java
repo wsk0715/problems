f82fb25f83f8bfa65f8f1def94cce03e5df789b7
wsk0715 <leindey@gmail.com>
2024-05-10 11:40:26 +0900

    ps: programmers 181950
    - lv.0 문자 반복해서 출력하기

---
+++ src/kr/co/programmers/school/solved/acceptance/Solution_181950.java
@@ -0,0 +1,38 @@
+package kr.co.programmers.school.solved.acceptance;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution_181950 {
+	// 문자 반복해서 출력하기
+	// https://school.programmers.co.kr/learn/courses/30/lessons/181950
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			String str = sc.next();
+			int n = sc.nextInt();
+
+			System.out.println(str.repeat(n));
+		}
+
+
+		// Libraries
+		private int[] toArray(ArrayList<Integer> al) {
+			// ArrayList<Integer> to int[]
+			int[] arr = new int[al.size()];
+			for (int i = 0; i < al.size(); i++) {
+				arr[i] = al.get(i);
+			}
+			return arr;
+		}
+
+	}
+
+}
