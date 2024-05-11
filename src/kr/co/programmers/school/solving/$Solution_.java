b379739e29fa1410cd42e999bfad001cb6b06f78
wsk0715 <leindey@gmail.com>
2024-05-11 14:27:52 +0900

    update ps template

--- src/kr/co/programmers/school/solving/$Solution_.java
+++ src/kr/co/programmers/school/solving/$Solution_.java
@@ -1,7 +1,5 @@
 package kr.co.programmers.school.solving;
 
-import java.util.ArrayList;
-
 public class $Solution_ {
 	//
 	// https://school.programmers.co.kr/learn/courses/30/lessons/
@@ -17,17 +15,6 @@ public class $Solution_ {
 			int answer = 0;
 
 			return answer;
-		}
-
-		
-		// Libraries
-		private int[] toArray(ArrayList<Integer> al) {
-			// ArrayList<Integer> to int[]
-			int[] arr = new int[al.size()];
-			for (int i = 0; i < al.size(); i++) {
-				arr[i] = al.get(i);
-			}
-			return arr;
 		}
 
 	}
