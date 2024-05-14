20e5faad494bf9b88d4881675306427ebf7b66c7
wsk0715 <leindey@gmail.com>
2024-05-14 11:23:07 +0900

    ps: programmers 181952
    - lv.0 문자열 출력하기

---
+++ src/kr/co/programmers/school/solved/acceptance/Solution_181952.java
@@ -0,0 +1,26 @@
+package kr.co.programmers.school.solved.acceptance;
+
+
+import java.util.Scanner;
+
+public class Solution_181952 {
+	// 문자열 출력하기
+	// https://school.programmers.co.kr/learn/courses/30/lessons/181952
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			String a = sc.next();
+
+			System.out.println(a);
+		}
+
+	}
+
+}
