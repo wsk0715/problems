720087544a216d69b4ea4fe0ff60c6652195db13
wsk0715 <leindey@gmail.com>
2024-05-14 11:22:58 +0900

    ps: programmers 181951
    - lv.0 a와 b 출력하기

---
+++ src/kr/co/programmers/school/solved/acceptance/Solution_181951.java
@@ -0,0 +1,27 @@
+package kr.co.programmers.school.solved.acceptance;
+
+import java.util.Scanner;
+
+public class Solution_181951 {
+	// a와 b 출력하기
+	// https://school.programmers.co.kr/learn/courses/30/lessons/181951
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+//		int[] input = {};
+//		sol.solution(input);
+	}
+
+	static class Solution {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			System.out.println("a = " + a);
+			System.out.println("b = " + b);
+		}
+
+	}
+
+}
