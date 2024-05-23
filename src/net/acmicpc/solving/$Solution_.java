436b51222d50306e4b7108d5d2f999e8800ca24b
wsk0715 <leindey@gmail.com>
2024-05-23 22:17:06 +0900

    etc: update ps template

--- src/net/acmicpc/solving/$Solution_.java
+++ src/net/acmicpc/solving/$Solution_.java
@@ -1,35 +1,53 @@
 package net.acmicpc.solving;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class $Solution_ {
-	private static final BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	//
 	// https://www.acmicpc.net/problem/
 	public static void main(String[] args) throws IOException {
 		int n = readInt();
 
+
+		close();
 	}
 
 
 	// Libraries
 	private static String readLine() throws IOException {
-		return bf.readLine();
+		return br.readLine();
 	}
 
 	private static int readInt() throws IOException {
-		int value = Integer.parseInt(bf.readLine().trim());
-		return value;
+		return Integer.parseInt(br.readLine()
+								  .trim());
 	}
 
 	private static int[] readIntegers() throws IOException {
-		return Arrays.stream(bf.readLine().trim().split(" "))
-				.mapToInt(Integer::parseInt)
-				.toArray();
+		return Arrays.stream(br.readLine()
+							   .trim()
+							   .split(" "))
+					 .mapToInt(Integer::parseInt)
+					 .toArray();
+	}
+
+	private static void write(String s) throws IOException {
+		bw.write(s);
+		bw.newLine();
+	}
+
+	private static void close() throws IOException {
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
