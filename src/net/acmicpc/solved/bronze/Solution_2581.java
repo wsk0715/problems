17fe8b72258553c8bf18100c295b2e754e9c81ba
wsk0715 <leindey@gmail.com>
2024-05-26 22:33:21 +0900

    ps: baekjoon 2581
    - Bronze II : 소수

---
+++ src/net/acmicpc/solved/bronze/Solution_2581.java
@@ -0,0 +1,81 @@
+package net.acmicpc.solved.bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution_2581 {
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	// 소수 - B2
+	// https://www.acmicpc.net/problem/2581
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		int m = readInt();
+
+
+		List<Integer> list = new ArrayList<>();
+		int sum = 0;
+		for (int i = n; i <= m; i++) {
+			boolean isPrime = true;
+			for (int j = 2; j <= Math.sqrt(i); j++) {
+				if (i % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime) {
+				if (i < 2) {
+					continue;
+				}
+				list.add(i);
+				sum += i;
+			}
+		}
+		if (sum != 0) {
+			write(sum);
+			write(list.get(0));
+		} else {
+			write(-1);
+		}
+		close();
+	}
+
+
+	// Libraries
+	private static String readLine() throws IOException {
+		return br.readLine();
+	}
+
+	private static int readInt() throws IOException {
+		return Integer.parseInt(br.readLine()
+								  .trim());
+	}
+
+	private static int[] readIntegers() throws IOException {
+		return Arrays.stream(br.readLine()
+							   .trim()
+							   .split(" "))
+					 .mapToInt(Integer::parseInt)
+					 .toArray();
+	}
+
+	private static <T> void write(T value) throws IOException {
+		String s = String.valueOf(value);
+		bw.write(s);
+		bw.newLine();
+	}
+
+	private static void close() throws IOException {
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
