a436400e8e0a1a722aa5f1b25a7ee579ae397b7e
wsk0715 <leindey@gmail.com>
2024-06-01 22:53:50 +0900

    ps: baekjoon 10808
    - Bronze IV : 알파벳 개수

---
+++ src/net/acmicpc/solved/bronze/Solution_10808.java
@@ -0,0 +1,61 @@
+package net.acmicpc.solved.bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Solution_10808 {
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	// 알파벳 개수 - B4
+	// https://www.acmicpc.net/problem/10808
+	public static void main(String[] args) throws IOException {
+		String S = readLine();
+
+		int[] arr = new int[26];
+		for (int i = 0; i < S.length(); i++) {
+			arr[S.charAt(i) - 'a'] += 1;
+		}
+		write(String.join(" ", Arrays.stream(arr)
+									 .mapToObj(String::valueOf)
+									 .toArray(String[]::new)));
+
+		close();
+	}
+
+
+	// Libraries
+	private static String readLine() throws IOException {
+		return br.readLine();
+	}
+
+	private static int readInt() throws IOException {
+		return Integer.parseInt(br.readLine()
+								  .trim());
+	}
+
+	private static int[] readIntegers() throws IOException {
+		return Arrays.stream(br.readLine()
+							   .trim()
+							   .split(" "))
+					 .mapToInt(Integer::parseInt)
+					 .toArray();
+	}
+
+	private static <T> void write(T value) throws IOException {
+		String s = String.valueOf(value);
+		bw.write(s);
+		bw.newLine();
+	}
+
+	private static void close() throws IOException {
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
