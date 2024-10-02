3c4fd3d0a95de64c1c789f6d78f44c3b0f295794
wsk0715 <leindey@gmail.com>
2024-10-02 23:58:25 +0900

    ps: programmers 340207
    - lv.0 문자 출력

---
+++ src/kr/co/programmers/school/solved/pcce/Solution_340207.java
@@ -0,0 +1,26 @@
+package kr.co.programmers.school.solved.pcce;
+
+public class Solution_340207 {
+
+	// 문자 출력
+	// https://school.programmers.co.kr/learn/courses/30/lessons/340207
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+
+		// #case 1
+		//		int[] n = {};
+		//		System.out.println(sol.solution(n));
+	}
+
+	static class Solution {
+
+		public static void main(String[] args) {
+			String message = "Let's go!";
+
+			System.out.println("3\n2\n1");
+			System.out.println(message);
+		}
+
+	}
+
+}
