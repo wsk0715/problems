cc5caef815e8f955a0b73e6d2b6f83df0e48c229
wsk0715 <leindey@gmail.com>
2025-03-20 22:45:19 +0900

    ps: baekjoon 10757
    - Bronze V : 큰 수 A+B

---
+++ src/net/acmicpc/solved/bronze/Solution_10757.java
@@ -0,0 +1,60 @@
+package net.acmicpc.solved.bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Solution_10757 {
+
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	// 큰 수 A+B - B5
+	// https://www.acmicpc.net/problem/10757
+	public static void main(String[] args) throws IOException {
+		String[] inputs = readLine().split(" ");
+		BigInteger result = new BigInteger(inputs[0]).add(new BigInteger(inputs[1]));
+
+		write(result);
+
+		close();
+	}
+
+
+	// Libraries
+	private static String readLine() throws IOException {
+		return br.readLine();
+	}
+
+	private static int readInt() throws IOException {
+		return Integer.parseInt(br.readLine());
+	}
+
+	private static int[] readIntegers() throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[st.countTokens()];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		return arr;
+	}
+
+	private static <T> void write(T value) throws IOException {
+		String s = String.valueOf(value);
+		bw.write(s);
+		bw.newLine();
+	}
+
+	private static void close() throws IOException {
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
