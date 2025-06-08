dfd040d22739e00b3f943c842123da4853a2229d
wsk0715 <leindey@gmail.com>
2025-06-08 23:10:28 +0900

    update template
    - 백준 파이썬 템플릿 수정

--- src/net/acmicpc/solving/$Solution_.py
+++ src/net/acmicpc/solving/$Solution_.py
@@ -4,16 +4,10 @@ import sys
 input = sys.stdin.readline
 
 
-# vars
-N, M = 0, 0
-
-
 #
 # https://www.acmicpc.net/problem/
 def main():
-    global N, M
     N = input()
 
 
-if __name__ == "__main__":
-    main()
+main()
