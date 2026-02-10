d00f5bacd8369f07d3d9059a4ba5ff2529d0486e
wsk0715 <leindey@gmail.com>
2026-02-10 17:11:08 +0900

    ps: swea 1230
    암호문3 (D3)

---
+++ src/com/swexpertacademy/solved/D3/Solution_1230.java
@@ -0,0 +1,85 @@
+package com.swexpertacademy.solved.D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution_1230 {
+
+	// 1230. 암호문3 (D3)
+	// https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14zIwqAHwCFAYD
+	public static void main(String[] args) throws IOException {
+		int T = 10;
+		for (int tc = 1; tc <= T; tc++) {
+			int N = nextInt();
+
+			List<Integer> ll = new LinkedList<>();
+			for (int i = 0; i < N; i++) {
+				ll.add(nextInt());
+			}
+
+			int M = nextInt();
+			for (int i = 0; i < M; i++) {
+				String command = nextToken();
+
+				if (command.equals("I")) {
+					int x = nextInt();
+					int y = nextInt();
+					for (int j = 0; j < y; j++) {
+						ll.add(x, nextInt());
+						x += 1;
+					}
+				} else if (command.equals("D")) {
+					int x = nextInt();
+					int y = nextInt();
+					for (int j = 0; j < y; j++) {
+						ll.remove(x);
+					}
+				} else if (command.equals("A")) {
+					int y = nextInt();
+					for (int j = 0; j < y; j++) {
+						ll.add(nextInt());
+					}
+				}
+			}
+
+
+			sb.append("#")
+			  .append(tc);
+			Iterator<Integer> it = ll.iterator();
+			for (int i = 0; i < 10; i++) {
+				sb.append(" ").append(it.next());
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+
+
+	// Libraries
+	private static String nextToken() throws IOException {
+		while (st == null || !st.hasMoreTokens()) {
+			String s = br.readLine();
+			if (s == null) {
+				return null;
+			}
+			st = new StringTokenizer(s);
+		}
+		return st.nextToken();
+	}
+
+	private static int nextInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+}
